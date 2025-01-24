7881253192d358c238fe69966624886fc6fecc03
ZiadElsewedy <ziadelsewedy1@gmai.com>
2025-01-25 01:12:31 +0200

    Generate model

--- Model/NoteModel.dart
+++
@@ -1,8 +0,0 @@
-class Note {
-  final String? Title ; 
-  final String? Subtitle ; 
-  final String? Date ;
-  final int? color;
-
-  Note({required this.color, required this.Title, required this.Subtitle, required this.Date}); 
-}
