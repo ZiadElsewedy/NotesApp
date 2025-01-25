ab52d817a38a381c83cf5c30795e2d0a3af92f5e
ZiadElsewedy <ziadelsewedy1@gmai.com>
2025-01-25 16:49:40 +0200

    Add notes States

--- lib/Cubits/AddNoteCubit/cubit/add_note_cubit_state.dart
+++ lib/Cubits/AddNoteCubit/cubit/add_note_cubit_state.dart
@@ -3,4 +3,8 @@ part of 'add_note_cubit_cubit.dart';
 @immutable
 sealed class AddNoteCubitState {}
 
-final class AddNoteCubitInitial extends AddNoteCubitState {}
+ class AddNoteCubitInitial extends AddNoteCubitState {}
+ class AddNoteCubitLoading extends AddNoteCubitState {}
+ class AddNoteCubitFailer extends AddNoteCubitState {}
+ class AddNoteCubitSucsses extends AddNoteCubitState {}
+
