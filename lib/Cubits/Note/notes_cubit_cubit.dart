e74627dec3e7aa06092901ed3f54a4b9500d0915
ZiadElsewedy <ziadelsewedy1@gmai.com>
2025-01-27 21:19:13 +0200

    fetch Notes

--- lib/Cubits/Note/notes_cubit_cubit.dart
+++ lib/Cubits/Note/notes_cubit_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notesapp/Model/Note_Model.dart';
+import 'package:notesapp/constants/constants.dart';
 
 part 'notes_cubit_state.dart';
 
 class NotesCubitCubit extends Cubit<NotesCubitState> {
   NotesCubitCubit() : super(NotesCubitInitial());
-}
+   FetchAllNotes() async {
+    emit(NotesCubitLoading());
+    try {
+      final notesBox = await Hive.box<NoteModel>(kNotesbox).values.toList();
+      emit(NotesCubitSuccess(notesBox));
+      
+    } catch (e) {
+      emit(NotesCubitFailure(e.toString()));
+    }
+}}
--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,35 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/Cubits/Note/notes_cubit_cubit.dart';
 import 'package:notesapp/Screens/AddNoteBottomSheet.dart';
 import 'package:notesapp/customs/CustomAppBar.dart';
 import 'package:notesapp/widgets/ListviewBuilder.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
-  static  String id = 'NotesView';
+  static String id = 'NotesView';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset:false ,
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        showModalBottomSheet(
-          isScrollControlled: true,
-          
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-          context: context, builder: (context){
-          return  AddNote();
-        });
-      } ,
-      child: Icon(Icons.add , color: Colors.white,), elevation: 10 , backgroundColor: Color.fromARGB(255, 95, 115, 130),),
-
-      appBar: AppBar(
-        title: CustomAppbar(Appbartitle: "Notes",icon: Icons.search,ontap: (){},)
-      ),
-      body: const Padding(
-        padding: EdgeInsets.only(right: 7 , left: 7 , top: 10 , bottom: 5),
-        child: SizedBox(
-          child: NotesListView()
+    return BlocProvider(
+      create: (context) => NotesCubitCubit()..FetchAllNotes(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18)),
+                context: context,
+                builder: (context) {
+                  return AddNote();
+                });
+          },
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
           ),
-          
+          elevation: 10,
+          backgroundColor: Color.fromARGB(255, 95, 115, 130),
+        ),
+        appBar: AppBar(
+            title: CustomAppbar(
+          Appbartitle: "Notes",
+          icon: Icons.search,
+          ontap: () {},
+        )),
+        body: const Padding(
+          padding: EdgeInsets.only(right: 7, left: 7, top: 10, bottom: 5),
+          child: SizedBox(child: NotesListView()),
+        ),
       ),
     );
   }
